5edf9ac7bc538dc5f700e1e334f8c5121c5936db
Mantas Kazlauskas <mmantas.kazlauskas@gmail.com>
2025-02-02 23:07:52 +0200

    moving everything into an addon folder pt2

---
+++ ChattyLittleNpc_Vanilla/ChattyLittleNpc_Vanilla.lua
@@ -0,0 +1,10 @@
+---@class ChattyLittleNpc_Vanilla
+local ChattyLittleNpc_Vanilla = LibStub("AceAddon-3.0"):NewAddon("ChattyLittleNpc_Vanilla")
+
+ChattyLittleNpc_Vanilla.Metadata = {
+    ContainsQuestVoiceovers = true,
+    ContainsGossipVoiceovers = false,
+    ContainsGameObjectVoiceovers = false,
+    ContainsItemVoiceovers = false
+}
+
